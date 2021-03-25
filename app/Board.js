ab6d13c962f4662a8d292d03d8d5306bdb9a65bf
Bharat <bjangid20>
2021-03-25 13:17:51 +0530

    Added icons on chess board also mapped to respective places

--- app/Board.js
+++ app/Board.js
@@ -1,4 +1,5 @@
 import Square from "./Square.js";
+import { Chess } from 'https://cdn.skypack.dev/chess.js';
 
 const files =  ['A', 'B', "C", "D", "E", "F", "J", "H"]
 
@@ -18,15 +19,19 @@ export default class Board{
       this.element.style.width = '90vmin';
       this.element.style.height = '90vmin';
     }
-
+    this.chess = new Chess();
+    this.board = this.chess.board().flat();
     this.cells = Array.from({length:64},(_,index) => {
       const rank = 8 - Math.floor(index/8);
-      const fileNum = index %8;
+      const fileNum = index % 8;
       const file = files[fileNum];
       const isBlack = !(rank%2 === fileNum%2);
-      const cell =  new Square({rank, file, isBlack});
+      const cell =  new Square({rank, file, isBlack, index, board: this});
       this.element.appendChild(cell.element);
       return cell;
     })
+  }
+  getSquare(index){
+    return this.board[index];
   }
 }
--- app/Square.js
+++ app/Square.js
@@ -1,7 +1,12 @@
+import Pieces from "./Pieces.js";
+
 export default class Square{
-  constructor({rank, file, isBlack}){
+  constructor({rank, file, isBlack, index, board}){
     this.rank = rank;
     this.file = file;
+    this.index = index;
+    this.board = board;
+    
     this.element = document.createElement('div');
     this.element.classList.add('square');
     if(isBlack){
@@ -10,5 +15,31 @@ export default class Square{
     this.element.textContent = `${file}${rank}`
     this.element.setAttribute('data-rank', rank);
     this.element.setAttribute('data-file', file);
+    this.update()
+  }
+  update(){
+    const current = this.board.getSquare(this.index);
+    if(current) {
+      const imageUrl = Pieces[`${current.color}${current.type}`]
+      if(imageUrl){
+        const image = document.createElement('img');
+        image.src = imageUrl;
+        this.element.textContent = '';
+        this.element.append(image);
+      }else{
+      this.element.textContent = current.type
+      }
+    }
+    if(!current) this.element.textContent = '';
+
+    if(this.rank === 1 || this.rank === 8){
+      const label = document.createElement('span');
+      label.classList.add('label');
+      if(this.rank === 8){
+        label.classList.add('top');
+      }
+      label.textContent = this.file;
+      this.element.append(label);
+    }
   }
 }
